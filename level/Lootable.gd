0903c5b416ee102da63330a3d62d4bc09b3839b1
outfrost <kotlet.bahn@gmail.com>
2021-04-25 15:24:48 +0200

    Add PlayerCharacter and Lootable scenes

--- level/Lootable.gd
+++ level/Lootable.gd
@@ -1,5 +1,5 @@
 class_name Lootable
-extends Node2D
+extends Area2D
 
 export(Item.ItemType) var loot1
 export var loot1_amount: int
@@ -9,8 +9,8 @@ export var loot2_amount: int
 var character
 
 func _ready() -> void:
-	$Area2D.connect("body_entered", self, "on_body_entered")
-	$Area2D.connect("body_exited", self, "on_body_exited")
+	connect("body_entered", self, "on_body_entered")
+	connect("body_exited", self, "on_body_exited")
 
 func on_body_entered(body):
 	if body.has_method("entered_lootable_range"):
